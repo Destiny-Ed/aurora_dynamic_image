a86450cfab5e54e9283c03513c5b4136c0e3af65
Destiny Dikeocha <destinyed@Destinys-MacBook-Pro.local>
2025-12-10 21:25:42 +0100

    added required dependencies

---
+++ lib/dynamic_image_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DynamicImageScreen extends StatefulWidget {
+  const DynamicImageScreen({super.key});
+
+  @override
+  State<DynamicImageScreen> createState() => _DynamicImageScreenState();
+}
+
+class _DynamicImageScreenState extends State<DynamicImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
